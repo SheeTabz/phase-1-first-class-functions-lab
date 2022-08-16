c98616bf0e2390da4ae77d963afe5dbfa16ecd8f
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-16 17:33:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers){
+  const newDrivers = drivers.slice(0,2)
+  return newDrivers
+
+}
+function returnLastTwoDrivers(){
+    const newDrivers = drivers.slice(2)
+  return newDrivers
+}
+// console.log(returnLastTwoDrivers())
+ 
+  const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    return function(fare){return fare * num}
+
+    }
+       
+function  fareDoubler(fare){
+   return fare * 2
+}
+function fareTripler(fare){
+   return fare * 3
+}
+function  selectDifferentDrivers(arrayOfDrivers, func){
+    return func(arrayOfDrivers)
+
+}
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
